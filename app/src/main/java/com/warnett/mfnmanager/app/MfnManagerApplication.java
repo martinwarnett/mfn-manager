353a9402f5ac07dfd7ba94455ec6f88fd89694f8
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2025-04-01 18:45:07 +0100

    MFNM-11 - Added App module along and updated .gitignore

---
+++ app/src/main/java/com/warnett/mfnmanager/app/MfnManagerApplication.java
@@ -0,0 +1,11 @@
+package com.warnett.mfnmanager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfnManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MfnManagerApplication.class, args);
+    }
+}
