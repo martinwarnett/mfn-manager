087af5198625aee46f63ddf524c651821fc91d46
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2025-04-07 14:33:33 +0100

    MFNM-15 : Added BaseIntegrationTEst class

---
+++ ui/src/test/java/integration/com/warnett/mfnmanager/ui/controller/BaseIntegrationTest.java
@@ -0,0 +1,18 @@
+package integration.com.warnett.mfnmanager.ui.controller;
+
+import com.warnett.mfnmanager.ui.utils.RenderedPageChecker;
+import com.warnett.mfnmanager.ui.utils.TestConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(classes = TestConfig.class)
+@AutoConfigureMockMvc
+public class BaseIntegrationTest {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected RenderedPageChecker pageChecker;
+}
