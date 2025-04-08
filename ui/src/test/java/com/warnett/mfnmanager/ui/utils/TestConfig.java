30c51ac7e3bc226efe76e99f58a861bd48c08ba2
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2025-04-08 09:35:20 +0100

    MFNM-15 : Added TestConfig for integration tests

---
+++ ui/src/test/java/com/warnett/mfnmanager/ui/utils/TestConfig.java
@@ -0,0 +1,8 @@
+package com.warnett.mfnmanager.ui.utils;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.warnett.mfnmanager.ui")
+public class TestConfig{}
