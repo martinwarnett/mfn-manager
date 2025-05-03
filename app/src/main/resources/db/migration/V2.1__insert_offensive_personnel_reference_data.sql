ceee4e518493e934d5ae9560edaa0b09a2fbcf73
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2025-05-03 11:42:49 +0100

    MFNM-59 : Created Offensive personnel reference data

---
+++ app/src/main/resources/db/migration/V2.1__insert_offensive_personnel_reference_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO reference_data.offensive_personnel(display_name, description, option_number)
+VALUES
+('OFF_005', '5WR', 0),
+('OFF_014', '1TE/4WR',1),
+('OFF_104', '1RB/4WR',10),
+('OFF_113', '1RB/1TE/3WR',11),
+('OFF_122', '1RB/2TE/2WR',12),
+('OFF_203', '2RB/3WR',20),
+('OFF_212', '2RB/1TE/2WR',21),
+('OFF_221', '2RB/2TE/1WR',22),
+('OFF_230', '2RB/3TE',23),
+('OFF_311', '3RB/1TE/1WR',31);
